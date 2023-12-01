865e1ef12df5073645f015abc1d7e7fbc43b374c
laxita <87566518+laxitajain@users.noreply.github.com>
2023-12-01 09:47:43 +0530

    Update e9t3.cpp optimised it.

--- e9t3.cpp
+++ e9t3.cpp
@@ -9,47 +9,43 @@ class student{
  float score;
  public:
  void read()
- { cout<<"Enter the name: ";
+ { cout<<"Name: ";
    cin>>name;
-   cout<<"Enter the Roll no. ";
+   cout<<"Roll no. ";
    cin>>rollno;
-   cout<<"Enter the marks of subject 1: ";
+   cout<<"Marks of subject 1: ";
    cin>>sub1;
-   cout<<"Enter the marks of subject 2: ";
+   cout<<"Marks of subject 2: ";
    cin>>sub2;
-   cout<<"Enter the marks of subject 3: ";
+   cout<<"Marks of subject 3: ";
    cin>>sub3;
    score=(sub1+sub2+sub3)/3;
-
  }
 
  void display()
- { if(score<40)
-    { cout<<"FAIL!"<<endl;
+ { 
       cout<<"Name: "<<name<<endl;
       cout<<"Roll No.:"<<rollno<<endl;
       cout<<"Subject 1 Marks: "<<sub1<<endl;
       cout<<"Subject 2 Marks: "<<sub2<<endl;
       cout<<"Subject 3 Marks: "<<sub3<<endl;
       cout<<"Score: "<<score<<endl;
+      if(score>40)
+       cout<<"Result: PASS!"<<endl;
+      else
+       cout<<"Result: FAIL"<<endl;
     }
-    else
-    { cout<<"PASS!"<<endl;
-      cout<<"Name: "<<name<<endl;
-      cout<<"Roll No.:"<<rollno<<endl;
-      cout<<"Subject 1 Marks: "<<sub1<<endl;
-      cout<<"Subject 2 Marks: "<<sub2<<endl;
-      cout<<"Subject 3 Marks: "<<sub3<<endl;
-      cout<<"Score: "<<score<<endl;
-    }
-}
- };
+};
 int main()
 { int i;
-  student s;
-  for(i=1;i<=10;i++)
-  { cout<<"Enter the details of student "<<i<<":"<<endl;
-    s.read(); s.display();
+  student s[3];
+  for(i=0;i<3;i++)
+  { cout<<"Enter the details of student "<<i+1<<":"<<endl;
+    s[i].read(); 
+  }
+ for(i=0;i<3;i++)
+  { cout<<"The details of student "<<i+1<<":"<<endl;
+    s[i].display(); 
   }
   return 0;
 }
